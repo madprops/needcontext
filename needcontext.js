ec510b75fa7b1c8ab192e7c650b14a2dfcf618b2
Auric Vente <mprops@protonmail.com>
2024-09-16 03:31:17 -0600

    Mods

--- needcontext.js
+++ needcontext.js
@@ -1,4 +1,4 @@
-// NeedContext v6.2
+// NeedContext v7.0
 
 // Main object
 const NeedContext = {}
@@ -487,7 +487,16 @@ NeedContext.select_action = async (e, index = NeedContext.index, mode = `mouse`)
 
   async function check_item () {
     if (item.action) {
-      NeedContext.action(item, e)
+      if (e.shiftKey) {
+        NeedContext.shift_action(item, e)
+      }
+      else if (e.ctrlKey) {
+        NeedContext.ctrl_action(item, e)
+      }
+      else {
+        NeedContext.action(item, e)
+      }
+
       return
     }
     else if (item.items) {
@@ -505,11 +514,23 @@ NeedContext.select_action = async (e, index = NeedContext.index, mode = `mouse`)
   }
 
   if (e.button === 0) {
-    check_item()
+    if (e.shiftKey) {
+      if (item.shift_action) {
+        NeedContext.shift_action(item, e)
+      }
+    }
+    else if (e.ctrlKey) {
+      if (item.ctrl_action) {
+        NeedContext.ctrl_action(item, e)
+      }
+    }
+    else {
+      check_item()
+    }
   }
   else if (e.button === 1) {
-    if (item.alt_action) {
-      NeedContext.alt_action(item, e)
+    if (item.middle_action) {
+      NeedContext.middle_action(item, e)
     }
   }
   else if (e.button === 2) {
@@ -705,10 +726,6 @@ NeedContext.init = () => {
       return
     }
 
-    if (NeedContext.modkey(e)) {
-      return
-    }
-
     NeedContext.keydown = true
 
     if (e.key === `ArrowUp`) {
@@ -733,10 +750,6 @@ NeedContext.init = () => {
     }
 
     if (!NeedContext.keydown) {
-      return
-    }
-
-    if (NeedContext.modkey(e)) {
       return
     }
 
@@ -898,11 +911,6 @@ NeedContext.clear_button = () => {
   return el
 }
 
-// Return true if a mod key is pressed
-NeedContext.modkey = (e) => {
-  return e.ctrlKey || e.altKey || e.shiftKey || e.metaKey
-}
-
 // Do an action
 NeedContext.action = (item, e) => {
   if (item.element) {
@@ -957,26 +965,58 @@ NeedContext.dismiss = (e) => {
   NeedContext.hide(e)
 }
 
-// Alternative action
-NeedContext.alt_action = (item, e) => {
+// Shift Click action
+NeedContext.shift_action = (item, e) => {
   if (item.element) {
     if (!NeedContext.is_visible(item.element)) {
       return
     }
   }
 
-  if (NeedContext.args.after_alt_action) {
-    NeedContext.args.after_alt_action(e)
+  if (NeedContext.args.after_shift_action) {
+    NeedContext.args.after_shift_action(e)
   }
 
-  if (NeedContext.args.alt_action_remove) {
+  NeedContext.hide(e)
+  item.shift_action(e)
+}
+
+// Ctrl Click action
+NeedContext.ctrl_action = (item, e) => {
+  if (item.element) {
+    if (!NeedContext.is_visible(item.element)) {
+      return
+    }
+  }
+
+  if (NeedContext.args.after_ctrl_action) {
+    NeedContext.args.after_ctrl_action(e)
+  }
+
+  NeedContext.hide(e)
+  item.ctrl_action(e)
+}
+
+// Middle Click action
+NeedContext.middle_action = (item, e) => {
+  if (item.element) {
+    if (!NeedContext.is_visible(item.element)) {
+      return
+    }
+  }
+
+  if (NeedContext.args.after_middle_action) {
+    NeedContext.args.after_middle_action(e)
+  }
+
+  if (NeedContext.args.middle_action_remove) {
     NeedContext.remove_item(item)
   }
   else {
     NeedContext.hide(e)
   }
 
-  item.alt_action(e)
+  item.middle_action(e)
 }
 
 // Context (right click) action
